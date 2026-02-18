903964406514ec6e7fd5056dd005617d533886b3
sllnoliveira <suellen.oliveira94@outlook.com>
2026-02-18 16:47:54 -0300

    Atualiza lógica de rankeamento

---
+++ index.js
@@ -0,0 +1,27 @@
+function calcularRank(vitorias, derrotas) {
+    
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias <= 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso
+let resultado = calcularRank(85, 10);
+console.log(resultado);
